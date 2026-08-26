242759a0471f3602e56255b20942c2c4b15f5a41
AlexisGo7 <alexiscastanomontoya@gmail.com>
2026-08-26 10:18:48 -0500

    Cambios de Estructura

--- app/src/main/java/com/example/myapplication/ui/screens/UserListScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/UserListScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.viewmodel.UserViewModel
 
 @Composable
