42386a9eb93d1fb04907a23378823ea30ed3bb0f
Aparicio-Munoz <daam2811@gmail.com>
2026-09-05 12:29:26 -0500

    feat: modernize animated login screen

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -3,18 +3,19 @@ package com.example.myapplication.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Brand
-val Accent = Color(0xFFF97316)
+val Accent = Color(0xFFFF6B00)
 val AccentHover = Color(0xFFEA580C)
-val AccentContainer = Color(0xFFFFEDD5)
+val AccentContainer = Color(0xFFFFE4CC)
 val OnAccentContainer = Color(0xFF7C2D12)
+val GlowPurple = Color(0xFF8B5CF6)
 
-// Dark palette from the web design tokens
-val Dark950 = Color(0xFF050A14)
-val Dark900 = Color(0xFF0A0F1E)
-val Dark800 = Color(0xFF0F172A)
-val Dark700 = Color(0xFF1E293B)
+// Dark palette for the SGTM login design
+val Dark950 = Color(0xFF080E1C)
+val Dark900 = Color(0xFF111827)
+val Dark800 = Color(0xFF191F2E)
+val Dark700 = Color(0xFF2B3445)
 val Dark600 = Color(0xFF334155)
-val Dark400 = Color(0xFF94A3B8)
+val Dark400 = Color(0xFF858A99)
 val Dark300 = Color(0xFFCBD5E1)
 
 // Light palette from the web design tokens
@@ -30,7 +31,7 @@ val Success = Color(0xFF059669)
 val SuccessBackground = Color(0xFFD1FAE5)
 val Warning = Color(0xFFD97706)
 val WarningBackground = Color(0xFFFEF3C7)
-val Danger = Color(0xFFDC2626)
+val Danger = Color(0xFFEF4444)
 val DangerBackground = Color(0xFFFEE2E2)
 val Info = Color(0xFF2563EB)
 val InfoBackground = Color(0xFFDBEAFE)
