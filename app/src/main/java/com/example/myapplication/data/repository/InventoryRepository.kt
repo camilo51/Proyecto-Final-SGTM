e996b88f013a972f06eb880ad9fd1cfe75dbc46e
Maylee Gomez Yarce <mayleegomez789@gmail.com>
2026-09-16 15:24:59 -0500

    Archivo InventoryRepository completo

--- app/src/main/java/com/example/myapplication/data/repository/InventoryRepository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/InventoryRepository.kt
@@ -1,4 +1,29 @@
 package com.example.myapplication.data.repository
 
-class InventoryRepository {
+import com.example.myapplication.data.api.ApiService
+import com.example.myapplication.data.model.Inventory
+
+open class InventoryRepository(
+    private val apiService: ApiService
+) {
+
+    open suspend fun getInventory(): List<Inventory> {
+        return apiService.getInventory()
+    }
+
+    open suspend fun getInventoryItem(id: String): Inventory {
+        return apiService.getInventoryItem(id)
+    }
+
+    open suspend fun createInventoryItem(inventory: Inventory): Inventory {
+        return apiService.createInventoryItem(inventory)
+    }
+
+    open suspend fun updateInventoryItem(id: String, inventory: Inventory): Inventory {
+        return apiService.updateInventoryItem(id, inventory)
+    }
+
+    open suspend fun deleteInventoryItem(id: String) {
+        apiService.deleteInventoryItem(id)
+    }
 }
