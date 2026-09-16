eb474786a195e9e623816aef11c6c5303fe34f1e
Maylee Gomez Yarce <mayleegomez789@gmail.com>
2026-09-16 15:40:36 -0500

    Contenido del archivo MotorcycleRepository

--- app/src/main/java/com/example/myapplication/data/repository/MotorcycleRepository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/MotorcycleRepository.kt
@@ -1,4 +1,29 @@
 package com.example.myapplication.data.repository
 
-class MotorcycleRepository {
+import com.example.myapplication.data.api.ApiService
+import com.example.myapplication.data.model.Motorcycle
+
+open class MotorcycleRepository(
+    private val apiService: ApiService
+) {
+
+    open suspend fun getMotorcycles(): List<Motorcycle> {
+        return apiService.getMotorcycles()
+    }
+
+    open suspend fun getMotorcycle(id: String): Motorcycle {
+        return apiService.getMotorcycle(id)
+    }
+
+    open suspend fun createMotorcycle(motorcycle: Motorcycle): Motorcycle {
+        return apiService.createMotorcycle(motorcycle)
+    }
+
+    open suspend fun updateMotorcycle(id: String, motorcycle: Motorcycle): Motorcycle {
+        return apiService.updateMotorcycle(id, motorcycle)
+    }
+
+    open suspend fun deleteMotorcycle(id: String) {
+        apiService.deleteMotorcycle(id)
+    }
 }
