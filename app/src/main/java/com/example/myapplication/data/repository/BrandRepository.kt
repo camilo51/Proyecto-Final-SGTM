25a8914541642a6071382ec39d3ce06fd471977e
Maylee Gomez Yarce <mayleegomez789@gmail.com>
2026-09-16 15:13:20 -0500

    Contenido del archivo BrandRepository

--- app/src/main/java/com/example/myapplication/data/repository/BrandRepository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/BrandRepository.kt
@@ -1,4 +1,25 @@
 package com.example.myapplication.data.repository
 
-class BrandRepository {
+import com.example.myapplication.data.api.ApiService
+import com.example.myapplication.data.model.Brand
+
+open class BrandRepository(
+    private val apiService: ApiService
+) {
+
+    open suspend fun getBrands(): List<Brand> {
+        return apiService.getBrands()
+    }
+
+    open suspend fun createBrand(brand: Brand): Brand {
+        return apiService.createBrand(brand)
+    }
+
+    open suspend fun updateBrand(id: String, brand: Brand): Brand {
+        return apiService.updateBrand(id, brand)
+    }
+
+    open suspend fun deleteBrand(id: String) {
+        apiService.deleteBrand(id)
+    }
 }
