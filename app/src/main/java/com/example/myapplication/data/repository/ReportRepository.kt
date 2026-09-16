cbfccfae0e96bb885890f3b51e8749e8d2bbc64c
Maylee Gomez Yarce <mayleegomez789@gmail.com>
2026-09-16 15:50:20 -0500

    Ultimo archivo de la carpeta repository, ReportRepository

--- app/src/main/java/com/example/myapplication/data/repository/ReportRepository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/ReportRepository.kt
@@ -1,4 +1,17 @@
 package com.example.myapplication.data.repository
 
-class ReportRepository {
+import com.example.myapplication.data.api.ApiService
+import com.example.myapplication.data.model.Report
+
+open class ReportRepository(
+    private val apiService: ApiService
+) {
+
+    open suspend fun getReports(): List<Report> {
+        return apiService.getReports()
+    }
+
+    open suspend fun getReport(id: String): Report {
+        return apiService.getReport(id)
+    }
 }
